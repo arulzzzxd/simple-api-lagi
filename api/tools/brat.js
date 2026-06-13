8f96012a5a19ce05e7c75ed7d224ab5f3dbfe57b
arulzzzxd <arulzz.xd@gmail.com>
2026-06-14 03:48:49 +0700

    Update brat.js

--- api/tools/brat.js
+++ api/tools/brat.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createCanvas, registerFont } = require('@napi-rs/canvas');
+const { createCanvas, registerFont, loadImage } = require('canvas');
 const fs = require('fs-extra');
 const path = require('path');
 
